aef5f17e665ff48e8b417ab0adcf61930f0358e4
IMCH222 <chukwuim@mail.uc.edu>
2023-06-04 11:13:08 -0400

    Work

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args) {
+        int intOperandA = 10;
+        int intOperandB = 5;
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+        double doubleOperandA = 5.50;
+        double doubleOperandB = 4.50;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of" +" " + intOperandA + " " + "&" + " " + intOperandB + " " + "is" + " " + intSum);
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product using ints of"+ " " + intOperandA + " " + "&" + " " + intOperandB + " " + "is" + " " + intProduct);
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The differnce using ints of" + " " + intOperandA + " " + "&" + " " + intOperandB + " " + "is" + " " + intDifference);
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient using ints of" + " " + intOperandA + " " + "&" + " " + intOperandB + " " + "is" + " " + intQuotient);
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo using ints of" + " " +  intOperandA + " " + "&" + " " + intOperandB + " " + "is" + " " + intModulo);
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of" + " " + doubleOperandA + " " + "&" + " " + doubleOperandB + " " + "is" + " " + doubleSum);
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product using doubles of" + " " + doubleOperandA + " " + "&" + " " + doubleOperandB + " " + "is" + " " + doubleProduct);
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference using doubles of" + " " + doubleOperandA + " " + "&" + " " + doubleOperandB + " " + "is" + " " + doubleDifference);
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient using doubles of" + " " + doubleOperandA + " " + "&" + " " + doubleOperandB + " " + "is" + " " + doubleQuotient);
+        int howManyKids = 3;
+        boolean isRaining = false;
+        double gasPrice = 3.50;
+        int favNum = 21;
+        int shoeSize = 13;
+        String birthMonth = "January";
+        String fullName = "Ikenna Chukwudolue";
+
+
+
+
+
+
+
+    }
+}
